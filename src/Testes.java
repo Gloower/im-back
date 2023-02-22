b098f5ef38f0014ac671fdf9af86b93e47411a75
Gloower <gloowerzin@gmail.com>
2023-02-21 21:47:13 -0300

    eae meu caro

--- src/Testes.java
+++ src/Testes.java
@@ -1,5 +1,5 @@
 public class Testes {
     public static void main(String[] args) {
-        System.out.println("Eae meu cria");
+        System.out.println("Fazendo alguns testes pro git");
     }
 }
